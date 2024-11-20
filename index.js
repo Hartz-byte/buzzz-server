1ffc6b2b03d29ef743721a0344fb81deba9f70e0
Harsh Gupta <harshmail281199@gmail.com>
2024-11-20 19:26:28 +0530

    env variable code updated

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 // index.ts
 const app_1 = __importDefault(require("./app"));
-const env_1 = require("./env");
+// const env_1 = require("./env");
 const database_1 = require("./config/database");
 // Connect to MongoDB
 (0, database_1.connectToDatabase)()
-    .then(() => {
-    app_1.default.listen(env_1.PORT, () => {
-        console.log(`GraphQL server is running at http://localhost:${env_1.PORT}/graphql`);
+  .then(() => {
+    app_1.default.listen(process.env.PORT, () => {
+      console.log(
+        `GraphQL server is running at http://localhost:${process.env.PORT}/graphql`
+      );
     });
-})
-    .catch((error) => {
+  })
+  .catch((error) => {
     console.error("Failed to connect to the database", error);
     process.exit(1);
-});
+  });
